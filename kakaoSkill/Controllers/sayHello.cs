828aab431325d9b43afd2131fb0e5a5f0065682a
mam675 <mam675@naver.com>
2021-06-29 18:12:48 +0900

    초기 커밋.

---
+++ kakaoSkill/Controllers/sayHello.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace kakaoSkill.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class sayHello : ControllerBase {
+        // GET: api/<sayHello>
+        [HttpGet]
+        public IEnumerable<string> Get() {
+            return new string[] { "value1000", "value2" };
+        }
+
+        // GET api/<sayHello>/5
+        [HttpGet("{id}")]
+        public string Get(int id) {
+            return "value";
+        }
+
+        // POST api/<sayHello>
+        [HttpPost]
+        public dynamic Post(dynamic value) {
+            Dictionary<string, object> response = new Dictionary<string, object>();
+
+            try {
+                Dictionary<string, object> text = new Dictionary<string, object>();
+                Dictionary<string, object> simpleText = new Dictionary<string, object>();
+                List<Dictionary<string, object>> outputs = new List<Dictionary<string, object>>();
+                Dictionary<string, object> template = new Dictionary<string, object>();
+
+                text.Add("text", "오토봇 발진");
+                //text.Add("포켓몬", "자 이제 시작이야 내꿈을~");
+                simpleText.Add("simpleText", text);
+                outputs.Add(simpleText);
+
+                template.Add("outputs", outputs);
+                response.Add("version", "2.0");
+                response.Add("template", template);
+            }
+            catch (Exception e) {
+
+            }
+
+            return response;
+        }
+
+        // PUT api/<sayHello>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) {
+        }
+
+        // DELETE api/<sayHello>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id) {
+        }
+    }
+}
