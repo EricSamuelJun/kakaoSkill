898842a725ddbbdb1a6290fec0559c73ad4c06e8
mam675 <mam675@naver.com>
2021-07-01 18:46:04 +0900

    21.07.01 인풋데이터 클래스화

--- kakaoSkill/Controllers/sayHello.cs
+++ kakaoSkill/Controllers/sayHello.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,20 +14,30 @@ namespace kakaoSkill.Controllers {
         // GET: api/<sayHello>
         [HttpGet]
         public IEnumerable<string> Get() {
+            Console.WriteLine("");
+            Console.WriteLine("======================================");
+            Console.WriteLine("\nGetMethod Call");
+            Console.WriteLine("======================================");
+            Console.WriteLine("");
             return new string[] { "value1000", "value2" };
         }
 
         // GET api/<sayHello>/5
         [HttpGet("{id}")]
         public string Get(int id) {
+            Console.WriteLine("");
+            Console.WriteLine("======================================");
+            Console.WriteLine("\nGetMethod Call");
+            Console.WriteLine("======================================");
+            Console.WriteLine("");
             return "value";
         }
 
         // POST api/<sayHello>
         [HttpPost]
-        public dynamic Post(dynamic value) {
+        public dynamic Post(JObject value) {
+            
             Dictionary<string, object> response = new Dictionary<string, object>();
-
             try {
                 Dictionary<string, object> text = new Dictionary<string, object>();
                 Dictionary<string, object> simpleText = new Dictionary<string, object>();
@@ -49,14 +60,52 @@ namespace kakaoSkill.Controllers {
             return response;
         }
 
-        public string CreateText(string val) {
-            string text = "Default";
-            return text;
+        public void CheckText(JObject value) {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            try {
+                Console.WriteLine(value.ToString());
+            }
+            catch(Exception ex) {
+                Console.WriteLine("Error @ 1");
+            }
+            try {
+                Console.WriteLine(value["userRequest"]["utterance"].ToString());
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Error @ 2");
+            }
+            try {
+                Console.WriteLine(value["userRequest"]["user"]["id"].ToString());
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Error @ 3");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
         }
-
+        public string FindWithKeyString(JObject jobj, string key) {
+            if (jobj.ContainsKey(key)) {
+                return jobj[key].ToString();
+            }
+            return "";
+        }
+        public JObject FindWithKeyObject(JObject jobj, string key) {
+            if (jobj.ContainsKey(key)) {
+            }
+            return null;
+        }
         // PUT api/<sayHello>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value) {
+            Console.WriteLine("");
+            Console.WriteLine("======================================");
+            Console.WriteLine("\nPut Mehtod Call with id: {0}, and value: {1}",id,value);
+            Console.WriteLine("======================================");
+            Console.WriteLine("");
         }
 
         // DELETE api/<sayHello>/5
---
+++ kakaoSkill/InputData.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kakaoSkill {
+    public class InputData {
+
+    }
+    public class Bot : Block {
+        public Bot(string a, string b) : base(a, b) {
+
+        }
+    }
+    public class Intent {
+        public string id;
+        public string name;
+        public IntentExtra extra;
+    }
+    public class IntentExtra {
+        public class reason {
+            public int code;
+            public string message;
+            public reason(int c, string m) { code = c; message = m; }
+        }
+        public reason mreason;
+        public string responseType;
+        public List<Knowledge> knowledges;
+        public IntentExtra(int code = -1, string message = "OK",string mrestype = "",List<Knowledge> knowledges = null) {
+            if (code == -1)
+                return;
+            mreason = new reason(code,message);
+            responseType = mrestype;
+            this.knowledges = knowledges;
+        }
+    }
+    
+    public class Knowledge {
+        public string answer;
+        public string question;
+        public List<string> categories;
+        public string landingUrl;
+        public string imageUrl;
+    }
+    public class Block {
+        public string id;
+        public string name;
+        public Block(string mid, string mname) {
+            id = mid;
+            name = mname;
+        }
+    }
+    public class UserRequest {
+        string timeZone;
+        Block block;
+        string utterance;
+        string lang;
+        User user;
+        Dictionary<string, string> p;
+    }
+    public class User {
+        string id;
+        string type;
+        Dictionary<string, string> properties;
+        public User(string mid, string mtype, Dictionary<string,string> mp = null) {
+            id = mid;
+            type = mtype;
+            if (mp != null)
+                properties = mp;
+        }
+    }
+}
