3c416b5f53a26be34a642ca3f081392e0cef4eb8
mam675 <mam675@naver.com>
2021-07-05 16:56:20 +0900

    블록 호출 샘플 추가함

--- kakaoSkill/Controllers/sayHello.cs
+++ kakaoSkill/Controllers/sayHello.cs
@@ -57,6 +57,20 @@ namespace kakaoSkill.Controllers {
                         , description: "당신은 죄악이 등골을 타고 오르는 것을 느꼈다.",
                         buttons: btns);
                     response = cr.get();
+                } else if (d.Utterance.IndexOf("펄럭") != -1) {
+                    List<Button> btns = new List<Button>();
+                    btns.Add(new Button(label: "블럭 테스트",action: Button.BUTTONACTION.block, messageText: "test 블록 호출",blockId: "60dacdf675f74b7865cc78f7"));
+                    btns.Add(new Button(label: "상담사 연결",action: Button.BUTTONACTION.operatorr));
+                    btns.Add(new Button(label: "url 연결", action: Button.BUTTONACTION.webLink, webLinkUrl: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"));
+
+                    CardResponse cr = new CardResponse(
+                        thumbnail: new Thumbnail(
+                            url: "https://w.namu.la/s/43a07e65f573eb41fffe67ac0d1008fa73b5c7a04a004ff9004ddf0680524c5c5bd8a30c724fd7966bd7d3a2f60d0bd17c3cc159dd41f704f9b6dc188a21346d43ba30ae041949e44e3a5fc57c739e3c363bded57e1423e1536dfdba3fc4364b"
+                            , link: new Link(web: "https://www.youtube.com/watch?v=n6WaTObHRJM&ab_channel=%EC%97%90%EB%A0%88%EB%A9%9Clmlxiabeize"))
+                        , title: "펄~럭"
+                        , description: "동해물과 백두산이 마르고 닳도록~",
+                        buttons: btns);
+                    response = cr.get();
                 } else {
                     
                     SimpleTextResponse str = new SimpleTextResponse("지난번 말: "+ DATA.temp + "\n" + "이번 말: " + d.Utterance);
